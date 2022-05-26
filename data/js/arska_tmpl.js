81424c14f509780964f1132015cfbddc167b51d3
Olli Rinne <olli@rinne.fi>
2022-05-26 10:55:43 +0300

    data folder updated

--- data/js/arska_tmpl.js
+++ data/js/arska_tmpl.js
@@ -1,4 +1,7 @@
-const sections = [{ "url": "/", "en": "Dashboard" }, { "url": "/inputs", "en": "Inputs" }
+/*
+Copyright, Netgalleria Oy, Olli Rinne 2021-2022
+*/
+const sections = [{ "url": "/", "en": "Dashboard" }, { "url": "/inputs", "en": "Services" }
     , { "url": "/channels", "en": "Channels" }, { "url": "/admin", "en": "Admin" }];
 
 const opers = JSON.parse('%OPERS%');
@@ -7,6 +10,7 @@ const CHANNEL_COUNT = parseInt('%CHANNEL_COUNT%'); //parseInt hack to prevent au
 const CHANNEL_CONDITIONS_MAX = parseInt('%CHANNEL_CONDITIONS_MAX%');
 const RULE_STATEMENTS_MAX = parseInt('%RULE_STATEMENTS_MAX%');
 const channels = JSON.parse('%channels%');  //moving data (for UI processing ) 
+const lang = '%lang%';
 
 
 // https://stackoverflow.com/questions/7317273/warn-user-before-leaving-web-page-with-unsaved-changes
@@ -15,6 +19,16 @@ var setFormSubmitting = function () {
     formSubmitting = true;
 };
 
+//disable/enable UI elements depending on varible mode (source/replica)
+function setVariableMode(variable_mode) {
+    document.getElementById("entsoe_api_key").disabled = (variable_mode != 0);
+    document.getElementById("entsoe_area_code").disabled = (variable_mode != 0);
+    document.getElementById("forecast_loc").disabled = (variable_mode != 0);
+
+    document.getElementById("variable_server").disabled = (variable_mode != 1);
+}
+
+
 function statusCBClicked(elCb) {
     //if (elCb.checked) {
     setTimeout(function () { updateStatus(elCb.checked); }, 1000);
@@ -22,8 +36,8 @@ function statusCBClicked(elCb) {
     document.getElementById("variables").style.display = document.getElementById("statusauto").checked ? "block" : "none";
 }
 
+// update variables and channels statuses to channels form
 function updateStatus(show_variables = true) {
-
     $.getJSON('/status', function (data) {
         if (show_variables) {
             document.getElementById("variables").style.display = document.getElementById("statusauto").checked ? "block" : "none";
@@ -59,9 +73,17 @@ function show_channel_status(channel_idx, is_up) {
     // snprintf(buff2,90,  "<svg viewBox='0 0 100 100' style='height:3em;'><use href='%s' id='status_%d'/></svg>",s.ch[channel_idx].is_up ? "#green" : "#red",channel_idx);
 }
 
+// before submitting input admin form
+var submitInputsForm = function (e) {
+    if (!confirm("Save and restart."))
+        return false;
+    formSubmitting = true;
+    return true;
+}
+
+// Before submitting channels config form
 var submitChannelForm = function (e) {
     // e.preventDefault();
-
     if (!confirm("Save channel settings?"))
         return false;
 
@@ -120,12 +142,11 @@ var submitChannelForm = function (e) {
     for (var i = 0; i < disSels.length; i++) {
         disSels[i].disabled = false;
     }
-
-
     formSubmitting = true;
-
     return true;
 };
+
+
 var isDirty = function () { return true; }
 
 function getVariable(variable_id) {
@@ -135,6 +156,8 @@ function getVariable(variable_id) {
     }
     return null;
 }
+
+// Add option to a given select element
 function addOption(el, value, text, selected = false) {
     var opt = document.createElement("option");
     opt.value = value;
@@ -143,7 +166,6 @@ function addOption(el, value, text, selected = false) {
     // then append it to the select element
     el.appendChild(opt);
 }
-
 
 
 
@@ -159,7 +181,6 @@ function populateStmtField(varFld, stmt = [-1, -1, 0]) {
     document.getElementById(varFld.id.replace("var", "op")).style.display = "none";
 
     advanced_mode = document.getElementById("mo_" + channel_idx + "_0").checked;
-
 
     addOption(varFld, -2, "remove");
     addOption(varFld, -1, "select", (stmt[0] == -1));
@@ -229,7 +250,10 @@ function addStmt(elBtn, channel_idx = -1, cond_idx = 1, stmt_idx = -1, stmt = [-
     const sel_var = createElem("select", "var" + suffix, null, "fldstmt indent", null);
     sel_var.addEventListener("change", setVar);
     const sel_op = createElem("select", "op" + suffix, null, "fldstmt", null);
-    sel_op.on_change = 'setOper(this)';
+
+    sel_op.addEventListener("change", setOper);
+
+
     const inp_const = createElem("input", "const" + suffix, 0, "fldtiny fldstmt inpnum", "text");
     const div_std = createElem("div", "std" + suffix, -2147483648, "divstament", "text");
     div_std.appendChild(sel_var);
@@ -249,20 +273,29 @@ function populateTemplateSel(selEl, template_id = -1) {
     addOption(selEl, -1, "select", false);
     $.getJSON('/data/template-list.json', function (data) {
         $.each(data, function (i, row) {
-            addOption(selEl, row["id"], row["name"], (template_id == row["id"]));
+            addOption(selEl, row["id"], _ltext(row,"name"), (template_id == row["id"]));
         });
     });
 }
 
+// unused ?
 function saveVal(el) {
-    $.data(el, 'current', $(el).val());
-    // alert($.data(el, 'current'));
-    // $(selEl.id).data('lastValue',$(this).val());   
+    $.data(el, 'current', $(el).val()); 
 }
+
+//localised text
+function _ltext(obj, prop) {
+    if (obj.hasOwnProperty(prop + '_' + lang))
+        return obj[prop + '_' + lang];
+    else if (obj.hasOwnProperty(prop))
+        return obj[prop];
+    else
+        return '['+prop+']';
+ }
 
 function templateChanged(selEl) {
     const fldA = selEl.id.split("_");
-    channel_idx = fldA[1];
+    channel_idx = parseInt(fldA[1]);
     console.log(selEl.value, channel_idx);
     template_idx = selEl.value;
     url = '/data/templates?id=' + template_idx;
@@ -272,7 +305,7 @@ function templateChanged(selEl) {
             deleteStmtsUI(channel_idx);
             return true;
         }
-        if (!confirm('Use template ' + data.name + ' - ' + data.desc)) {
+        if (!confirm('Use template ' + _ltext(data,"name") + ' - ' + _ltext(data,"desc"))) {
             $(selEl).val($.data(selEl, 'current'));
             return false;
         }
@@ -410,13 +443,25 @@ function setVar(evt) {
     }
 
 }
-function setOper(el) {
 
+// operator select changed, show next statement fields if hidden
+function setOper(evt) {
+    const el = evt.target;
+    if ((el.value >= 0)) {
+        fldA = el.id.split("_");
+        channel_idx = parseInt(fldA[1]);
+        cond_idx = parseInt(fldA[2]);
+        // show initially hidden rules
+        if ((cond_idx + 1) < CHANNEL_CONDITIONS_MAX) {
+            document.getElementById("ru_" + channel_idx + "_" + (cond_idx + 1)).style.display = "block";
+        }
+    }
 }
 
 
-function setEnergyMeterFields(val) { //
-    idx = parseInt(val);
+function setEnergyMeterFields(emt) { //
+
+    idx = parseInt(emt);
 
     var emhd = document.querySelector('#emhd');
     var empd = document.querySelector('#empd');
@@ -448,6 +493,7 @@ function setEnergyMeterFields(val) { //
 function fillStmtRules(channel_idx, rule_mode) {
     console.log("fillStmtRules", channel_idx, rule_mode);
 
+    prev_rule_var_defined = true;
     for (let cond_idx = 0; cond_idx < CHANNEL_CONDITIONS_MAX; cond_idx++) {
         firstStmtVarId = "var_" + channel_idx + "_" + cond_idx + "_0";
         firstStmtVar = document.getElementById(firstStmtVarId);
@@ -459,6 +505,16 @@ function fillStmtRules(channel_idx, rule_mode) {
         //    first_var_defined = (firstStmtVar.value >= 0);
 
         show_rule = (rule_mode == 0) || first_var_defined;
+        // testing hiding , hide empty rules to save space
+        if (rule_mode == 0) {
+            if (cond_idx == 0)
+                show_rule = true;
+            else {
+                show_rule = prev_rule_var_defined;
+            }
+        }
+        prev_rule_var_defined = first_var_defined;
+
 
         //just debug
         if (first_var_defined)
@@ -488,8 +544,6 @@ function initChannelForm() {
         //TODO: fix if more types coming
         chtype = document.getElementById('chty_' + channel_idx).value;
         setChannelFieldsByType(channel_idx, chtype);
-
-
     }
 
     let stmts_s = document.querySelectorAll("input[id^='stmts_']");
@@ -542,16 +596,15 @@ function initChannelForm() {
 function setChannelFieldsByType(ch, chtype) {
     for (var t = 0; t < RULE_STATEMENTS_MAX; t++) {
         divid = 'td_' + ch + "_" + t;
-
         var uptimediv = document.querySelector('#d_uptimem_' + ch);
-
         if (chtype == 0)
             uptimediv.style.display = "none";
         else
             uptimediv.style.display = "block";
-
     }
 }
+
+
 function initUrlBar(url) {
     var headdiv = document.getElementById("headdiv");
 
@@ -588,15 +641,17 @@ function initUrlBar(url) {
     // <a href="/">Dashboard</a> | <span> <b>Admin</b> </span>
 
 }
-
 //
-
-
 
 function initForm(url) {
     initUrlBar(url);
     if (url == '/admin') {
         initWifiForm();
+        // set language select element
+        var lang = document.getElementById("lang_db").value;
+        $('#lang option').filter(function () {
+            return this.value.indexOf(lang) > -1;
+        }).prop('selected', true);
     }
     else if (url == '/channels') {
         initChannelForm();
@@ -605,13 +660,46 @@ function initForm(url) {
         setTimeout(function () { updateStatus(false); }, 3000);
     }
 
+    else if (url == '/inputs') {
+        if (document.getElementById("VARIABLE_SOURCE_ENABLED").value == 0) {
+            variable_mode = 1;
+            document.getElementById("variable_mode_0").disabled = true;
+        }
+        else {
+            variable_mode = document.getElementById("variable_mode_db").value;
+        }
+
+        setVariableMode(variable_mode);
+        document.getElementById("variable_mode_" + variable_mode).checked = true;
+
+        setEnergyMeterFields(document.getElementById("emt").value);
+
+        //set forecast location select element
+        var location = document.getElementById("forecast_loc_db").value;
+        $('#forecast_loc option').filter(function () {
+            return this.value.indexOf(location) > -1;
+        }).prop('selected', true);
+
+        //set forecast location select element
+        var area_code = document.getElementById("entsoe_area_code_db").value;
+        $('#entsoe_area_code option').filter(function () {
+            return this.value.indexOf(area_code) > -1;
+        }).prop('selected', true);
+
+
+        // show influx definations only if enbaled by server
+        if (document.getElementById("INFLUX_REPORT_ENABLED").value != 1) {
+            document.getElementById("influx_div").style.display = "none";
+        }
+    }
 
     var footerdiv = document.getElementById("footerdiv");
     if (footerdiv) {
-        footerdiv.innerHTML = "<a href='http://netgalleria.fi/rd/?arska-wiki' target='arskaw'>Arska Wiki</a> | <a href='http://netgalleria.fi/rd/?arska-states' target='arskaw'>States</a> | <a href='http://netgalleria.fi/rd/?arska-rulesets'  target='arskaw'>Rulesets</a>";
+        footerdiv.innerHTML = "<a href='http://netgalleria.fi/rd/?arska-wiki' target='arskaw'>Arska Wiki</a> ";
     }
 }
 
+// for sorting wifis by signal strength
 function compare_wifis(a, b) {
     return ((a.rssi > b.rssi) ? -1 : 1);
 }
@@ -624,8 +712,6 @@ function initWifiForm() {
         if (wifi.id) {
             var opt = document.createElement("option");
             opt.value = wifi.id;
-            // if (wifi.id == wifi_ssid) //from constants via template processing
-            //     opt.selected = true;
             opt.innerHTML = wifi.id + ' (' + wifi.rssi + ')';
             wifi_sel.appendChild(opt);
         }
@@ -641,6 +727,7 @@ function setChannelFields(obj) {
     setChannelFieldsByType(ch, chtype);
 }
 
+// check admin form fields before form post 
 //beforeAdminSubmit
 function checkAdminForm() {
     document.querySelector('#ts').value = Date.now() / 1000;
@@ -657,9 +744,11 @@ function checkAdminForm() {
     return true;
 }
 
+// remove?
 function clearText(elem) {
     elem.value = '';
 }
+
 // Ruleset processing
 function processRulesetImport(evt) {
     if (!evt.target.id.startsWith("rules_"))
@@ -707,8 +796,10 @@ function processRulesetImport(evt) {
         }
     }
 }
+
 let rs1 = document.getElementById('rs1')
+/*
 document.addEventListener('paste', e => {
     processRulesetImport(e);
 })
-
+*/
