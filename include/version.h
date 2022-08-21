663fb535c1a03b99f039c47c7c74667fa74d208d
Olli Rinne <olli@rinne.fi>
2022-08-21 20:23:04 +0300

    new branch

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "1271"
+  #define BUILD_NUMBER "1273"
 #endif
 #ifndef VERSION
-  #define VERSION "0.91.1271 - 2022-08-18 23:16:38"
+  #define VERSION "0.91.1273 - 2022-08-21 20:22:26"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "0.91.1271"
+  #define VERSION_SHORT "0.91.1273"
 #endif
