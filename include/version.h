919ef975e0d33997d1b1448229060779ad39c94b
Olli <olli@rinne.fi>
2022-09-09 10:17:40 +0300

    just new version

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "534"
+  #define BUILD_NUMBER "540"
 #endif
 #ifndef VERSION
-  #define VERSION "0.92.534 - 2022-09-09 08:03:24"
+  #define VERSION "0.92.540 - 2022-09-09 10:09:23"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "0.92.534"
+  #define VERSION_SHORT "0.92.540"
 #endif
