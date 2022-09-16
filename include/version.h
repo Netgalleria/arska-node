ebb7193f23bee2669ea96a075e98ebe9b6c21d0b
Olli <olli@rinne.fi>
2022-09-16 21:19:45 +0300

    readme

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "549"
+  #define BUILD_NUMBER "550"
 #endif
 #ifndef VERSION
-  #define VERSION "0.92.549 - 2022-09-15 14:13:29"
+  #define VERSION "0.92.550 - 2022-09-16 21:01:25"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "0.92.549"
+  #define VERSION_SHORT "0.92.550"
 #endif
