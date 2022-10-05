60bbb586a4a625f4abcccf8ef5ec41096fcb23ea
Olli <olli@rinne.fi>
2022-10-05 17:58:20 +0300

    version

--- include/version.h
+++ include/version.h
@@ -3,12 +3,12 @@
     #define VERSION_BASE "0.92.0-rc2"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "1181"
+    #define BUILD_NUMBER "1182"
   #endif
   #ifndef VERSION
-    #define VERSION "0.92.0-rc2.1181 - 2022-10-03 21:23:40"
+    #define VERSION "0.92.0-rc2.1182 - 2022-10-05 17:56:44"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "0.92.0-rc2.1181"
+    #define VERSION_SHORT "0.92.0-rc2.1182"
   #endif
   
