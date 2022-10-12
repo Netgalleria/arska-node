a78fca978015f7066f8453d47c6e301605536f08
Olli <olli@rinne.fi>
2022-10-12 12:37:14 +0300

    Synch

--- include/version.h
+++ include/version.h
@@ -1,10 +1,14 @@
-
-#ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "927"
-#endif
-#ifndef VERSION
-  #define VERSION "0.92.927 - 2022-10-05 17:02:04"
-#endif
-#ifndef VERSION_SHORT
-  #define VERSION_SHORT "0.92.927"
-#endif
+//0.92.0-rc2
+  #ifndef VERSION_BASE
+    #define VERSION_BASE "0.92.0-rc2"
+  #endif
+  #ifndef BUILD_NUMBER
+    #define BUILD_NUMBER "1184"
+  #endif
+  #ifndef VERSION
+    #define VERSION "0.92.0-rc2.1184 - 2022-10-12 12:05:45"
+  #endif
+  #ifndef VERSION_SHORT
+    #define VERSION_SHORT "0.92.0-rc2.1184"
+  #endif
+  
