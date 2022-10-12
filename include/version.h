a0bde6967785aa03615d2b3c4dfa06950f1e85d2
Olli <olli@rinne.fi>
2022-10-12 15:08:36 +0300

    new build

--- include/version.h
+++ include/version.h
@@ -3,12 +3,12 @@
     #define VERSION_BASE "0.92.0-rc2"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "1235"
+    #define BUILD_NUMBER "1274"
   #endif
   #ifndef VERSION
-    #define VERSION "0.92.0-rc2.1235 - 2022-10-11 23:47:41"
+    #define VERSION "0.92.0-rc2.1274 - 2022-10-12 15:03:06"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "0.92.0-rc2.1235"
+    #define VERSION_SHORT "0.92.0-rc2.1274"
   #endif
   
