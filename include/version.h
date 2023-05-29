fe95b339c3da045573b95fbf9695d3b75b442c3b
Olli <olli@rinne.fi>
2023-05-29 16:16:52 +0300

    Version and init

--- include/version.h
+++ include/version.h
@@ -1,14 +1,14 @@
-//0.99.0-alfa3
+//0.99.0-alfa4
   #ifndef VERSION_BASE
-    #define VERSION_BASE "0.99.0-alfa3"
+    #define VERSION_BASE "0.99.0-alfa4"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "3071"
+    #define BUILD_NUMBER "3087"
   #endif
   #ifndef VERSION
-    #define VERSION "0.99.0-alfa3.3071 - 2023-05-28 16:32:42"
+    #define VERSION "0.99.0-alfa4.3087 - 2023-05-29 11:40:41"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "0.99.0-alfa3.3071"
+    #define VERSION_SHORT "0.99.0-alfa4.3087"
   #endif
   
