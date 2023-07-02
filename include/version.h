ba3161e370037f1894b9a7cbd5fb0321bc63c074
Olli <olli@rinne.fi>
2023-07-02 12:40:25 +0300

    0.99.0-alfa8 for new modbus library

--- include/version.h
+++ include/version.h
@@ -1,14 +1,14 @@
-//0.99.0-alfa7
+//0.99.0-alfa8
   #ifndef VERSION_BASE
-    #define VERSION_BASE "0.99.0-alfa7"
+    #define VERSION_BASE "0.99.0-alfa8"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "3293"
+    #define BUILD_NUMBER "3309"
   #endif
   #ifndef VERSION
-    #define VERSION "0.99.0-alfa7.3293 - 2023-06-29 18:25:14"
+    #define VERSION "0.99.0-alfa8.3309 - 2023-07-02 12:29:09"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "0.99.0-alfa7.3293"
+    #define VERSION_SHORT "0.99.0-alfa8.3309"
   #endif
   
