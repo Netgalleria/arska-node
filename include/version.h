5fdd405baf73d8cacf0e86c2ede88725460d8f9d
Olli <olli@rinne.fi>
2024-01-13 09:19:55 +0200

    Energy meter gpio max fixed

--- include/version.h
+++ include/version.h
@@ -1,15 +1,15 @@
-//1.1.0-alfa2
+//1.1.0-alfa3
   #ifndef VERSION_BASE
-    #define VERSION_BASE "1.1.0-alfa2"
+    #define VERSION_BASE "1.1.0-alfa3"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "4331"
+    #define BUILD_NUMBER "4506"
   #endif
   #ifndef VERSION
-    #define VERSION "1.1.0-alfa2.4331 - 2023-12-28 13:51:30"
+    #define VERSION "1.1.0-alfa3.4506 - 2024-01-13 09:16:04"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "1.1.0-alfa2.4331"
+    #define VERSION_SHORT "1.1.0-alfa3.4506"
   #endif
   #ifndef ESP_IDF_VERSION_CUSTOM
     #define ESP_IDF_VERSION_CUSTOM "4.4.5"
