646cd9559f08d5c09bba6fb0c67087527f26e629
Olli <olli@rinne.fi>
2024-05-23 14:33:10 +0300

    New template 501 for solar overprod & forecast

--- include/version.h
+++ include/version.h
@@ -1,15 +1,15 @@
-//1.1.0-beta4
+//1.1.0-alfa7
   #ifndef VERSION_BASE
-    #define VERSION_BASE "1.1.0-beta4"
+    #define VERSION_BASE "1.1.0-alfa7"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "5176"
+    #define BUILD_NUMBER "5215"
   #endif
   #ifndef VERSION
-    #define VERSION "1.1.0-beta4.5176 - 2024-05-05 10:46:16"
+    #define VERSION "1.1.0-alfa7.5215 - 2024-05-23 14:14:50"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "1.1.0-beta4.5176"
+    #define VERSION_SHORT "1.1.0-alfa7.5215"
   #endif
   #ifndef ESP_IDF_VERSION_CUSTOM
     #define ESP_IDF_VERSION_CUSTOM "4.4.5"
