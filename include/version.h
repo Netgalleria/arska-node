21fb1897602f64f2dd339f71ef74217049a41486
Olli <olli@rinne.fi>
2025-01-23 16:30:30 +0200

    version 1.1.0-rc2

--- include/version.h
+++ include/version.h
@@ -1,15 +1,15 @@
-//1.1.0-beta12
+//1.1.0-rc2
   #ifndef VERSION_BASE
-    #define VERSION_BASE "1.1.0-beta12"
+    #define VERSION_BASE "1.1.0-rc2"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "5555"
+    #define BUILD_NUMBER "5561"
   #endif
   #ifndef VERSION
-    #define VERSION "1.1.0-beta12.5555 - 2025-01-09 12:41:58"
+    #define VERSION "1.1.0-rc2.5561 - 2025-01-23 16:18:07"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "1.1.0-beta12.5555"
+    #define VERSION_SHORT "1.1.0-rc2.5561"
   #endif
   #ifndef ESP_IDF_VERSION_CUSTOM
     #define ESP_IDF_VERSION_CUSTOM "4.4.5"
