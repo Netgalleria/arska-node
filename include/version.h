54e65114eb8729ee5a50e8b91ff1a9747f5f55de
Olli <olli@rinne.fi>
2025-06-09 19:52:42 +0300

    versions

--- include/version.h
+++ include/version.h
@@ -1,15 +1,15 @@
-//1.3.0-beta3
+//1.3.0-beta4
   #ifndef VERSION_BASE
-    #define VERSION_BASE "1.3.0-beta3"
+    #define VERSION_BASE "1.3.0-beta4"
   #endif
   #ifndef BUILD_NUMBER
-    #define BUILD_NUMBER "6388"
+    #define BUILD_NUMBER "6586"
   #endif
   #ifndef VERSION
-    #define VERSION "1.3.0-beta3.6388 - 2025-05-15 17:23:49"
+    #define VERSION "1.3.0-beta4.6586 - 2025-06-09 19:52:22"
   #endif
   #ifndef VERSION_SHORT
-    #define VERSION_SHORT "1.3.0-beta3.6388"
+    #define VERSION_SHORT "1.3.0-beta4.6586"
   #endif
   #ifndef ESP_IDF_VERSION_CUSTOM
     #define ESP_IDF_VERSION_CUSTOM "4.4.5"
